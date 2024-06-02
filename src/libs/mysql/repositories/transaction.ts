2cf165a1eb9c30734d345a2817169abb63dbb055
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 22:02:11 +0800

    Implement transaction repository

---
+++ src/libs/mysql/repositories/transaction.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
+import { Transaction } from '../entities';
+import { TransactionMapper } from '@/mappers';
+
+@Injectable()
+class TransactionRepository {
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly _transactionRepo: Repository<Transaction>,
+  ) {}
+
+  async exists(options: FindOptionsWhere<Transaction>) {
+    return this._transactionRepo
+      .count({ where: options })
+      .then((count) => count > 0);
+  }
+
+  async findOne(options: FindOptionsWhere<Transaction>) {
+    const user = await this._transactionRepo.findOne({
+      where: options,
+    });
+    if (!user) {
+      return user;
+    }
+    return TransactionMapper.toDomain(user);
+  }
+  async create(account: Transaction) {
+    this._transactionRepo.save(account);
+    return TransactionMapper.toDomain(account);
+  }
+}
+
+export default TransactionRepository;
