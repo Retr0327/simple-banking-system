b552508fc8e7c05972270f7f2998abdfbce8452e
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 21:59:44 +0800

    Remove transaction enum

--- src/libs/mysql/entities/index.ts
+++ src/libs/mysql/entities/index.ts
@@ -1,7 +1,7 @@
 import type { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 import { Account } from './account.entity';
-import { Transaction, TransactionType } from './transaction.entity';
+import { Transaction } from './transaction.entity';
 
 const entities: EntityClassOrSchema[] = [Account, Transaction] as const;
 
-export { Account, Transaction, TransactionType, entities };
+export { Account, Transaction, entities };
