85f4dc25f817566de19a8657ed629d8534c943a8
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 21:42:27 +0800

    Export controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+import AccountController from './accounts/account.controller';
+import TransactionController from './transactions/transaction.controller';
+
+const controllers = [AccountController, TransactionController];
+
+export { controllers };
