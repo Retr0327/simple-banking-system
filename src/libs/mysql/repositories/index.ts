656b069dfc06dc75d4054d0b8d879635e2dabf52
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 22:01:54 +0800

    Export MySQL repositories

---
+++ src/libs/mysql/repositories/index.ts
@@ -0,0 +1,6 @@
+import AccountRepository from './account';
+import TransactionRepository from './transaction';
+
+const repositories = [AccountRepository, TransactionRepository];
+
+export { AccountRepository, TransactionRepository, repositories };
