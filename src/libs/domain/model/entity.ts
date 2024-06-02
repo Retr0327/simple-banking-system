a9442516be22282d326b8b52a346751935996b89
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 21:57:16 +0800

    Define abstract domain entity

---
+++ src/libs/domain/model/entity.ts
@@ -0,0 +1,11 @@
+export abstract class Entity<Id, Props> {
+  readonly id: Id;
+  protected props: Props;
+
+  protected constructor(id: Id, props: Props) {
+    this.id = id;
+    this.props = props;
+  }
+
+  public abstract equals(obj?: Entity<Id, Props>): boolean;
+}
