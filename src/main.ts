9fea83e0e68f713db385101aed16403e310e5445
Retr0327 <retr0327.dev@gmail.com>
2024-06-02 22:18:09 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.use(helmet());
+  app.setGlobalPrefix('api/v1');
+  await app.listen(3000);
+}
+
+bootstrap();
