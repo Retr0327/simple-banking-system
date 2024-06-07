db0d3245e5339f9e3ac3605bf7b8bb10cf0d64ec
Retr0327 <retr0327.dev@gmail.com>
2024-06-07 16:49:14 +0800

    Import TypeORM module options for dynamic environment configuration

--- src/libs/mysql/mysql.module.ts
+++ src/libs/mysql/mysql.module.ts
@@ -1,27 +1,20 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config, { env } from '@/configs';
+import env from '@/configs';
 import { entities } from './entities';
 import { repositories } from './repositories';
+import { typeOrmModuleOptions } from './configs';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [config],
+      load: [env],
       cache: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: env.mysqlHost,
-      port: 3306,
-      username: env.mysqlUser,
-      password: env.mysqlPassword,
-      database: env.mysqlDb,
-      entities,
-    }),
+    TypeOrmModule.forRoot({ ...typeOrmModuleOptions, entities }),
     TypeOrmModule.forFeature(entities),
   ],
   providers: [...repositories],
